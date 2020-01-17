3ac64e877c7fdc6ab69df3e61f6a7ffb975ac72c
Ryan Westlund <rlwestlund@gmail.com>
2020-01-16 23:04:04 -0500

    also zap U+0489

--- unzalgo.py
+++ unzalgo.py
@@ -4,7 +4,7 @@ import sys
 output = ""
 
 for char in sys.stdin.read():
-	if not 0x0300 <= ord(char) <= 0x036F:
+	if not 0x0300 <= ord(char) <= 0x036F and not ord(char) == 0x0489:
 		output += char
 
 print(output)
