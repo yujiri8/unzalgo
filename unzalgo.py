826aed450c38a97a33a49c810765a2ce6ccd4d72
Ryan Westlund <rlwestlund@gmail.com>
2020-01-16 22:57:23 -0500

    Simplify code

--- unzalgo.py
+++ unzalgo.py
@@ -1,12 +1,10 @@
 #!/usr/local/bin/python3
 import sys
 
-contents = sys.stdin.read()
+output = ""
 
-new_contents = ""
-
-for char in contents:
+for char in sys.stdin.read():
 	if not 0x0300 <= ord(char) <= 0x036F:
-		new_contents += char
+		output += char
 
-print(new_contents)
+print(output)
